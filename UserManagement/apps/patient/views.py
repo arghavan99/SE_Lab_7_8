3dc2d6c64fd2e1c60e45f8cc5e6ac19ac91bf5fa
arghavan99 <55342129rez>
2021-12-16 13:32:51 +0330

    patient serializer and get_new_patients

--- UserManagement/apps/patient/views.py
+++ UserManagement/apps/patient/views.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND
+from rest_framework.renderers import JSONRenderer
 from django.shortcuts import render
 
 # Create your views here.
 from apps.patient.models import Patient
+from apps.patient.serializers import PatientSerializer
 
 
 def get_patient(request):
@@ -13,8 +15,15 @@ def get_patient(request):
     if p is None:
         return Response(status=HTTP_404_NOT_FOUND)
     p = p[0]
-    response = {
-        'name': p.name,
-        'email': p.email,
-    }
-    return JsonResponse(response, status=HTTP_200_OK)
+    serialized_patient = PatientSerializer(p)
+    print(serialized_patient.data)
+    res = JSONRenderer().render(serialized_patient.data)
+    return JsonResponse(res, status=HTTP_200_OK)
+
+
+def get_new_patients(request):
+    patients = reversed(Patient.objects.all().order_by('-id')[:5])
+    serialized_patients = PatientSerializer(patients, many=True)
+    print(serialized_patients.data)
+    res = JSONRenderer().render(serialized_patients.data)
+    return JsonResponse(res, status=HTTP_200_OK)
